a868c023d5d006c0005c2125323a79fb18b99483
Sébastien Guimmara <sguimmara@pobox.com>
2022-07-07 20:39:41 +0200

    very basic term output

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,37 @@ use std::{
     io::{self, Write},
 };
 
-// (Buf) Uncomment these lines to have the output buffered, this can provide
-// better performance but is not always intuitive behaviour.
-// use std::io::BufWriter;
-
 use structopt::StructOpt;
 
 // Our CLI arguments. (help and version are automatically generated)
 // Documentation on how to use:
 // https://docs.rs/structopt/0.2.10/structopt/index.html#how-to-derivestructopt
 #[derive(StructOpt, Debug)]
+#[structopt(name="cuiller", about = "Generate a QR code.")]
 struct Cli {
-    // The pattern we want to look for.
-    pattern: String,
-    // The path of the file we want to look at.
-    path: String,
+    /// The text to put into a QR code
+    text: String,
+
+    // #[structopt(short="o", long="output")]
+    // output: Option<String>,
 }
 
 fn main() {
     let args = Cli::from_args();
-    let contents = fs::read_to_string(&args.path)
-        .expect("Could not read file.");
-    let mut stdout = io::stdout();
+
+    // let mut stdout = io::stdout();
+    let source = args.text;
+
+    println!("generating QR code for string '{}'...", source);
+    // let contents = fs::read_to_string(&args.path)
+    //     .expect("Could not read file.");
     // (Buf) Wraps stdout in a buffer.
     // let mut stdout = BufWriter::new(stdout);
 
-    for (line_no, line) in contents.lines().enumerate() {
-        if line.contains(&args.pattern) {
-            writeln!(stdout, "{}: {}", line_no + 1, line);
-        }
-    }
+    // for (line_no, line) in contents.lines().enumerate() {
+    //     if line.contains(&args.pattern) {
+    //         writeln!(stdout, "{}: {}", line_no + 1, line);
+    //     }
+    // }
 }
 
